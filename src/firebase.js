7d6cbbb26d9638d28deedbb245ee3617501779f8
Bharathgit5 <bharathjbiet10@gmail.com>
2025-06-10 22:41:47 +0530

    removed test code

--- src/firebase.js
+++ src/firebase.js
@@ -10,11 +10,7 @@ import {
   onChildRemoved,
    // if you are using remove()
 } from "firebase/database";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
